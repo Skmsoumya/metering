aba54f1f62f3f3cdb3221fbd4f1dd134e3d332b2
Skmsoumya <soumyakishormishra@gmail.com>
2020-05-10 13:08:57 +0530

    adding tests for meter reading route (/meters/:meterId/readings)

--- backend/src/v1/routes/meters.router.test.js
+++ backend/src/v1/routes/meters.router.test.js
@@ -62,3 +62,45 @@ describe('Meters endpoint', () => {
     expect(res.body.length).toBe(0)
   })
 })
+
+describe('Meater\'s reading endpoint', () => {
+  it('should respond with a list of reading for each valid meter id', async () => {
+    const meterId = 'METER000001'
+    const res = await request(app)
+      .get(`/meters/${meterId}/readings`).send()
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body).not.toBeUndefined()
+    expect(res.body.meterId).toBe(meterId)
+    expect(res.body.period).toBe('last_month')
+
+    expect(typeof res.body.startTimestamp).toBe('number')
+    expect(res.body.startTimestamp).not.toBe(0)
+
+    expect(typeof res.body.endTimestamp).toBe('number')
+    expect(res.body.endTimestamp).not.toBe(0)
+
+    expect(typeof res.body.readings).toBe('array')
+
+    if (res.body.readings.length > 0) {
+      const aReading = res.body[0]
+      expect(aReading).not.toBeUndefined()
+
+      expect(typeof aReading.readingTimestampUnix).toBe('number')
+      expect(aReading.readingTimestampUnix).not.toBe(0)
+
+      expect(typeof aReading.wattHour).toBe('number')
+      expect(typeof aReading.varh).toBe('number')
+      expect(typeof aReading.changeInVARH).toBe('number')
+      expect(typeof aReading.changeInWH).toBe('number')
+    }
+  })
+
+  it('should respond with 404 error if meter not found on system', async () => {
+    const meterId = 'METER0000012'
+    const res = await request(app)
+      .get(`/meters/${meterId}/readings`).send()
+
+    expect(res.statusCode).toBe(404)
+  })
+})
